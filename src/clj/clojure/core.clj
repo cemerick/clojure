bb296edb1d231c98ea18d8ad7c32ade067e7c8da
Chas Emerick <cemerick@snowtide.com>
2009-06-17 17:44:25 -0400

    add encoding argument to slurp

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -2975,16 +2975,19 @@
   [v] (instance? clojure.lang.Var v))
 
 (defn slurp
-  "Reads the file named by f into a string and returns it."
-  [#^String f]
-  (with-open [r (new java.io.BufferedReader (new java.io.FileReader f))]
+  "Reads the file named by f using the encoding enc into a string and returns it."
+  ([f] (slurp f (-> java.nio.charset.Charset (. defaultCharset) (.name))))
+  ([#^String f #^String enc]
+  (with-open [r (new java.io.BufferedReader
+                  (new java.io.InputStreamReader
+                    (new java.io.FileInputStream f) enc))]
     (let [sb (new StringBuilder)]
       (loop [c (. r (read))]
         (if (neg? c)
           (str sb)
           (do
             (. sb (append (char c)))
-            (recur (. r (read)))))))))
+            (recur (. r (read))))))))))
 
 (defn subs
   "Returns the substring of s beginning at start inclusive, and ending
